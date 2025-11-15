61b46a1e571b6aa06e7e76284045b0e38d42936c
geraldi <geraldi19adityo@gmail.com>
2025-11-15 17:20:28 +0700

    (fix) testing fix 1

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -15,6 +15,28 @@ const nextConfig = {
                 destination: `${backendUrl}/:path*`
             }
         ]
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, must-revalidate'
+                    }
+                ]
+            },
+            {
+                source: '/_next/static/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    }
+                ]
+            }
+        ]
     }
 };
 
