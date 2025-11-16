d58d6e9939f98528d641a5c93be92d9028274825
geraldi <geraldi19adityo@gmail.com>
2025-11-16 15:54:09 +0700

    fix 12

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    generateBuildId: async () => {
-        if(process.env.GIT_HASH){
-            return process.env.GIT_HASH
-        }
-
-        return `build-${new Date().toISOString().slice(0,13)}`
-    },
     async rewrites(){
         const backendUrl = process.env.INTERNAL_BACKEND_API_URL;
         
@@ -23,28 +16,6 @@ const nextConfig = {
             }
         ]
     },
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'no-store, must-revalidate'
-                    }
-                ]
-            },
-            {
-                source: '/_next/static/:path*',
-                headers: [
-                    {
-                        key: 'Cache-Control',
-                        value: 'public, max-age=31536000, immutable',
-                    }
-                ]
-            }
-        ]
-    }
 };
 
 export default nextConfig;
