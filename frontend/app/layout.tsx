b584352af6b13b583f055f74e7c951905f1a3c0e
geraldi <geraldi19adityo@gmail.com>
2025-11-16 11:15:47 +0700

    (try fix 5)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import './globals.css'
+import './globals.css';
 import { Toaster } from "sonner";
 import { QueryProvider } from "@/components/provider/QueryProvider";
 import { ReduxProvider } from "@/components/provider/ReduxProvider";
-import { UpgradeHandler } from "@/components/UpgradeHandler";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
