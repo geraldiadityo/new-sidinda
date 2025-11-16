c6b638642bd353badad154d7210ad1fdcc6f9d1c
geraldi <geraldi19adityo@gmail.com>
2025-11-16 11:49:33 +0700

    (fix 6)

--- frontend/app/(root)/user/page.tsx
+++ frontend/app/(root)/user/page.tsx
@@ -1,23 +1,23 @@
-import UserWrapper from "@/components/master/user/UserWrapper";
+// import UserWrapper from "@/components/master/user/UserWrapper";
 
-const UserPage = ({searchParams}: {searchParams: { page?: string, pageSize?: string, search?: string}}) => {
-    const page = Number(searchParams.page) || 1;
-    const pageSize = Number(searchParams.pageSize) || 5;
-    const searchQuery = searchParams.search || '';
+// const UserPage = ({searchParams}: {searchParams: { page?: string, pageSize?: string, search?: string}}) => {
+//     const page = Number(searchParams.page) || 1;
+//     const pageSize = Number(searchParams.pageSize) || 5;
+//     const searchQuery = searchParams.search || '';
 
-    const initData = {
-        metadata: {
-            page,
-            pageSize,
-            searchQuery
-        }
-    };
+//     const initData = {
+//         metadata: {
+//             page,
+//             pageSize,
+//             searchQuery
+//         }
+//     };
 
-    return (
-        <div className="container mx-auto py-6">
-            <UserWrapper initData={initData} />
-        </div>
-    )
-}
+//     return (
+//         <div className="container mx-auto py-6">
+//             <UserWrapper initData={initData} />
+//         </div>
+//     )
+// }
 
-export default UserPage;
+// export default UserPage;
